7de0664370507c5f011ff6a38fadbc06fb28fbd0
JoseSolorzanoPardo <75858195+JoseSolorzanoPardo@users.noreply.github.com>
2025-05-18 16:09:44 -0500

    Update Modulo1.sql

--- EjerciciosCurso/Modulo1.sql
+++ EjerciciosCurso/Modulo1.sql
@@ -33,3 +33,72 @@ INSERT INTO Transacciones (TransaccionID, ClienteID, Fecha, Monto, Tipo, Canal) 
 (1005, 3, '2025-05-17 15:30:00', 80000.00, 'Transferencia', 'Web'),
 (1006, 4, '2025-05-17 16:00:00', 300000.00, 'Transferencia', 'App');
 
+INSERT INTO Transacciones (TransaccionID, ClienteID, Fecha, Monto, Tipo, Canal) VALUES
+(1007, 1, '2025-05-17 09:15:00', 5000.00, 'Consulta Saldo', 'App'),
+(1008, 1, '2025-05-17 09:30:00', 1500000.00, 'Transferencia', 'Web'), 
+(1009, 2, '2025-05-17 10:00:00', 200000.00, 'Pago', 'Web'),
+(1010, 2, '2025-05-17 10:15:00', 199000.00, 'Transferencia', 'App'),
+(1011, 2, '2025-05-17 10:17:00', 5000.00, 'Consulta Saldo', 'App'),
+(1012, 3, '2025-05-17 11:00:00', 60000.00, 'Retiro', 'Cajero'),
+(1013, 3, '2025-05-17 11:10:00', 800000.00, 'Transferencia', 'Web'), 
+(1014, 4, '2025-05-17 12:00:00', 10000.00, 'Pago', 'App'),
+(1015, 4, '2025-05-17 12:10:00', 20000.00, 'Pago', 'App'),
+(1016, 4, '2025-05-17 12:30:00', 15000.00, 'Consulta Saldo', 'Web');
+
+
+--Consultando la tabla de transacciones
+SELECT * FROM Transacciones ORDER BY Fecha DESC;
+
+--Determinando la cantidad de transacciones y tablas
+SELECT ClienteID, COUNT(*) AS NumTransacciones, SUM(Monto) AS TotalMovido
+FROM Transacciones
+GROUP BY ClienteID;
+
+--Conjunto de actualizaciones
+UPDATE Clientes SET Segmento = 'Premium' WHERE Nombre = 'Carlos Gómez';
+UPDATE Transacciones SET Canal = 'Cajero' WHERE TransaccionID = 1015;
+UPDATE Transacciones SET Monto = 80000.00 WHERE TransaccionID = 1013;
+
+--Eliminación de información
+DELETE FROM Transacciones WHERE Monto = 0.00;
+
+
+-- Primero verifica si no tiene transacciones
+SELECT * FROM Transacciones WHERE ClienteID = 99;
+-- Luego eliminas si está vacío
+DELETE FROM Clientes
+WHERE ClienteID = 99;
+
+--Total de transacciones por cliente
+SELECT 
+    ClienteID,
+    COUNT(*) AS TotalTransacciones,
+    SUM(Monto) AS MontoTotal
+FROM Transacciones
+GROUP BY ClienteID;
+
+--Monto total por tipo de transacción
+SELECT 
+    Tipo,
+    COUNT(*) AS Cantidad,
+    SUM(Monto) AS TotalPorTipo
+FROM Transacciones
+GROUP BY Tipo;
+
+--Transacciones por cliente y tipo de transacción
+SELECT 
+    ClienteID,
+    Tipo,
+    COUNT(*) AS Cantidad,
+    SUM(Monto) AS Total
+FROM Transacciones
+GROUP BY ClienteID, Tipo;
+
+--Clientes con más de 2 transacciones (con HAVING)
+SELECT 
+    ClienteID,
+    COUNT(*) AS TotalTransacciones
+FROM Transacciones
+GROUP BY ClienteID
+HAVING COUNT(*) > 2;
+
